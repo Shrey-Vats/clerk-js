6bd67d20a78fda5184d84b51632d3570abe48c06
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-06 20:53:01 +0530

    add: verify verification token logic

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   email: { type: String, require: true },
   password: { type: String, require: true },
-  role: { type: String, enum: ["admin", "user"], default: "user" },
+  role: {
+    type: String,
+    enum: ["admin", "user", "unVerified"],
+    default: "unVerified",
+  },
   verifyToken: { type: String },
   verifyTokenExpiry: { type: Date },
   verifyType: {
--- server/services/tokenService.js
+++ server/services/tokenService.js
@@ -3,16 +3,44 @@ import User from '../models/user';
 
 
 export const generateToken = async (userId, type) =>{
+    try {
+        const user = await User.findById(userId);
+        const token = await bcrypt.hash(user._id.toString(), 10);
+        const tokenExpiry = Date.now() + 1000 * 60 * 60 * 24 * 7; // 7 days
 
-    const user = await User.findById(userId)
-    const token = await bcrypt.hash(user._id.toString(), 10)
-    const tokenExpiry = Date.now() + 1000 * 60 * 60 * 24 * 7; // 7 days
+        user.verifyToken = token;
+        user.verifyTokenExpiry = tokenExpiry;
+        user.verifyType = type;
 
-    user.verifyToken = token;
-    user.verifyTokenExpiry = tokenExpiry;
-    user.verifyType = type;
+        await user.save();
 
+        return token;
+    } catch (error) {
+       console.log(error)
+       return null 
+    }
+}
+
+export const verifyToken = async (token, type) =>{
+    
+   try {
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+      verifyType: type,
+    });
+
+    if (!user) {
+      console.log("Token is not valid");
+      return null;
+    }
+
+    user.role = "user";
     await user.save();
 
-    return token
+    return user;
+   } catch (error) {
+    console.log(error);
+    return null
+   }
 }
