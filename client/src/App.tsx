25d66cbce7900c3ccf5fc760c0b85efba5ff13f3
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-07 23:12:49 +0530

    setup: shadcn library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
-import { Button } from "@/components/ui/button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SignupPage } from './pages/signup.tsx';
 
 function App() {
 
 
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <Router>
+      <Routes>
+     
+        <Route path='/' element={<SignupPage />} />
+      </Routes>
+    </Router>
   );
 }
 
