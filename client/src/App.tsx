d2b780b7725b889ab662b80a3482b48a6563773b
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-08 19:42:44 +0530

    add: sonner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import  {LoginPage } from "./pages/login.tsx";
 import { SignupPage } from './pages/signup.tsx';
+import VerifyEmailPage from './pages/verify.tsx';
+import OtpSentPage from './pages/OTPSendNotification.tsx';
+import { Toaster } from "sonner";
 
 function App() {
 
@@ -9,10 +12,12 @@ function App() {
   return (
     <Router>
       <Routes>
-     
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='/login' element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify" element={<VerifyEmailPage />} />
+        <Route path="/otp-sent" element={<OtpSentPage />} />
       </Routes>
+      <Toaster richColors position="top-center" />
     </Router>
   );
 }
